b6eccb2edb019004622537727f2d02b31d57c2df
Mgazul <mgazul@163.com>
2026-04-16 16:08:59 +0800

    Fixed #1

--- src/main/java/me/andandsf/advancedfurnace/AdvancedFurnaceScreenHandler.java
+++ src/main/java/me/andandsf/advancedfurnace/AdvancedFurnaceScreenHandler.java
@@ -29,7 +29,7 @@ public class AdvancedFurnaceScreenHandler extends AbstractContainerMenu {
         this.addDataSlots(propertyDelegate);
         int m,l;
 
-        this.addSlot(new Slot(inventory, 0, 12, 33){
+        this.addSlot(new Slot(inventory, 0, 12, 22){
             @Override
             public boolean mayPlace(ItemStack stack) {
                 return playerInventory.player.level().fuelValues().isFuel(stack) || stack.getItem() == Items.BUCKET;
@@ -44,17 +44,17 @@ public class AdvancedFurnaceScreenHandler extends AbstractContainerMenu {
             int inputSlot = 1 + i * 2;
             int outputSlot = 2 + i * 2;
 
-            this.addSlot(new Slot(inventory, inputSlot, 46 + 27 * i, 19));
-            this.addSlot(new AdvancedFurnaceResultSlot(playerInventory.player, inventory, outputSlot, 46 + 27 * i, 63));
+            this.addSlot(new Slot(inventory, inputSlot, 46 + 27 * i, 8));
+            this.addSlot(new AdvancedFurnaceResultSlot(playerInventory.player, inventory, outputSlot, 46 + 27 * i, 52));
         }
 
         for (m = 0; m < 3; ++m) {
             for (l = 0; l < 9; ++l) {
-                this.addSlot(new Slot(playerInventory, l + m * 9 + 9, 8 + l * 18, 95 + m * 18));
+                this.addSlot(new Slot(playerInventory, l + m * 9 + 9, 8 + l * 18, 84 + m * 18));
             }
         }
         for (m = 0; m < 9; ++m) {
-            this.addSlot(new Slot(playerInventory, m, 8 + m * 18, 153));
+            this.addSlot(new Slot(playerInventory, m, 8 + m * 18, 142));
         }
     }
 
